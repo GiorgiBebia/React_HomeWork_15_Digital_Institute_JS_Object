00e0d0455af25209625f4017c13dcedbac1aa171
GiorgiBebia <139773975+GiorgiBebia@users.noreply.github.com>
2024-07-04 17:12:47 +0400

    Task Done

---
+++ index.js
@@ -0,0 +1,15 @@
+const person = {
+  name: "Giorgi",
+  last_name: "Bebia",
+  phone_number: 598123456,
+  hobby: ["Computer Games", "Football", "Running"],
+};
+
+console.log(person);
+console.log(person.name);
+console.log(person.last_name);
+console.log(person.phone_number);
+console.log(person.hobby);
+console.log(person.hobby[0]);
+console.log(person.hobby[1]);
+console.log(person.hobby[2]);
